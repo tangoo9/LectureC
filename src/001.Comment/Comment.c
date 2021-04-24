8c5b29d5ce7825bc666fb0e4457059cde1c32c4e
김태우 <kokicola@naver.com>
2021-04-24 07:48:44 +0000

    주석 실습 추가

---
+++ src/001.Comment/Comment.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+	
+{
+	/* 	가
+		나
+		다
+		마
+		바 
+		*/
+	
+	//한줄 주석
+	printf("computer \n");
+	printf("hi wolrd! \n");
+	printf("hello wolrd! \n");   
+	
+	return 0;
+}
