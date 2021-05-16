2781f3b9ee18f9a398539aa016076e2a3a3fe9c2
김태우 <kokicola@naver.com>
2021-05-16 08:45:45 +0000

    00

--- src/011.Variable/11.StaticStoageClass.c
+++ src/011.Variable/11.StaticStoageClass.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
-static int count = 5;
-int main(){
-	while(count--){
-		func();
-	}
-	return 0;
-}
-void func(void){
-	static int i =5;
-	printf("i : %d, count : %d \n", i, count);
+#include <stdio.h> 
+void func(void); 
+static int count = 5; //정적 전역 변수
+int main() { 
+    while(count--) { 
+        func(); 
+    } 
+    
+    return 0; 
+} 
+
+void func( void ) { 
+    static int i = 5; //정적 지역 변수 
+    i++; 
+    printf("i : %d, count : %d\n", i, count); 
 }
